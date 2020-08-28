c2364cb7acf894c25f0307aff21c6298cf441226
Shaiah Wren <sde2111@columbia.edu>
2020-08-28 16:28:04 -0400

    js up to date

--- js/index.js
+++ js/index.js
@@ -1,121 +1,64 @@
 'use strict';
-
-
 let categoryType = 10;
 let questionNumber = 10;
 let questionDifficulty = 'medium';
-//let questionsArr = [];
-
-if (x === 0){
-    document.addEventListener('DOMContentLoaded', () =>{
-        console.log('this works')
-        //
-        const getQuestionNum = () => {
-            const questionNum = document.getElementById('questionNum')
-            for (let i=1; i <16; i++){
-                const numOption = document.createElement('option')
-                numOption.value = i.toString();
-                numOption.text = i.toString();
-                questionNum.appendChild(numOption);
-                questionNumber = i;
-            }
+let x = 0;
+document.addEventListener('DOMContentLoaded', () =>{
+    localStorage.removeItem('questions')
+    localStorage.removeItem('correctCounter')
+    console.log('this works')
+    const getQuestionNum = () => {
+        const questionNum = document.getElementById('questionNum')
+        for (let i=1; i <16; i++){
+            const numOption = document.createElement('option')
+            numOption.value = i.toString();
+            numOption.text = i.toString();
+            questionNum.appendChild(numOption);
         }
-        getQuestionNum()
-        const getQuestionDifficulty = () => {
-            const questionDiff = document.getElementById('difficulty')
-            const arrDiff = ["Easy", "Medium", "Hard"]
-            arrDiff.map(function(diff){
-                const diffOption = document.createElement('option');
-                questionDiff.appendChild(diffOption);
-                diffOption.value = diff;
-                diffOption.text = diff;
-                questionDifficulty = diff;
-            })
-        }
-        getQuestionDifficulty()
-        const getCategories = () => {
-            const url = `https://opentdb.com/api_category.php`;
-            const categorySelect = document.getElementById('category')
-        
-            get(url).then(function(categories){
-                const arr = categories.trivia_categories
-                arr.map(function(category){
-                    const categoryOption = document.createElement('option')
-                    categorySelect.appendChild(categoryOption)
-                    categoryOption.value = category.name;
-                    categoryOption.text = category.name;
-                    categoryType = category.id;
-                    
-                })
-            })
-        }
-        getCategories()
-    })
-}
-
-getQuestionNum()
-const getQuestionDifficulty = () => {
-    const questionDiff = document.getElementById('difficulty')
-    const arrDiff = ["Easy", "Medium", "Hard"]
-    arrDiff.map(function(diff){
-        const diffOption = document.createElement('option');
-        questionDiff.appendChild(diffOption);
-        diffOption.value = diff;
-        diffOption.text = diff;
-        questionDifficulty = diff;
-    })
-}
-getQuestionDifficulty()
-const getCategories = () => {
-    const url = `https://opentdb.com/api_category.php`;
-    const categorySelect = document.getElementById('category')
-
-    get(url).then(function(categories){
-        const arr = categories.trivia_categories
-        arr.map(function(category){
-            const categoryOption = document.createElement('option')
-            categorySelect.appendChild(categoryOption)
-            categoryOption.value = category.name;
-            categoryOption.text = category.name;
-            categoryType = category.id;
-            
-        })
-    })
-}
-getCategories()
-
-const submitButton = document.querySelector('#submitForm')
-
-submitButton.addEventListener('click', e =>{
-<<<<<<< HEAD
-    const submitButtonPressed = () => {
-=======
-    let questionsArr = [];
-    console.log(questionsArr)
-    //const submitButtonPressed = () => {
->>>>>>> 195d749634ed09b278dc03b194f169ea54674088
-        // const url = `https://opentdb.com/api.php?amount=${questionNumber}&category=${categoryType}&difficulty=${questionDifficulty}`
-        const url = `https://opentdb.com/api.php?amount=10&category=10&difficulty=medium`
-        get(url).then(questions =>{
-            console.log(questions.results[0].correct_answer)
-            questionsArr = questions.results
-            console.log(questionsArr)
-<<<<<<< HEAD
-=======
-            x = 1;
-            //HTMLBody.innerHTML = questionPageHTML
-            //updateButtons()
-            console.log('after')
-            localStorage.setItem("questions", JSON.stringify(questionsArr))
-            window.location.replace('gamescreen.html')
->>>>>>> 195d749634ed09b278dc03b194f169ea54674088
+    }
+    getQuestionNum()
+    const getQuestionDifficulty = () => {
+        const questionDiff = document.getElementById('difficulty')
+        const arrDiff = ["Easy", "Medium", "Hard"]
+        arrDiff.map(function(diff){
+            const diffOption = document.createElement('option');
+            questionDiff.appendChild(diffOption);
+            diffOption.value = diff;
+            diffOption.text = diff;
         })
     }
-<<<<<<< HEAD
-    submitButtonPressed()
+    getQuestionDifficulty()
+    const getCategories = () => {
+        const url = `https://opentdb.com/api_category.php`;
+        const categorySelect = document.getElementById('category')
+        get(url).then(function(categories){
+            const arr = categories.trivia_categories
+            arr.map(function(category){
+                const categoryOption = document.createElement('option')
+                categorySelect.appendChild(categoryOption)
+                categoryOption.value = category.id;
+                categoryOption.text = category.name;
+            })
+        })
+    }
+    getCategories()
+})
+const submitButton = document.querySelector('#submitForm')
+submitButton.addEventListener('click', e =>{
+    const categorySelect = document.getElementById('category')
+    const questionDiff = document.getElementById('difficulty')
+    const questionNum = document.getElementById('questionNum')
+    let questionsArr = [];
+    const url = `https://opentdb.com/api.php?amount=${questionNum.value}&category=${categorySelect.value}&difficulty=${questionDiff.value.toLowerCase()}&type=multiple`
+    console.log(url)
+    get(url).then(questions =>{
+        console.log(questions.results[0].correct_answer)
+        questionsArr = questions.results
+        console.log(questionsArr)
+        x = 1;
+        console.log('after')
+        localStorage.setItem("questions", JSON.stringify(questionsArr))
+        window.location.replace('gamescreen.html')
+    })
 });
 
-=======
-    //submitButtonPressed()
-);
->>>>>>> 195d749634ed09b278dc03b194f169ea54674088
